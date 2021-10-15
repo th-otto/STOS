6f0fc4957863dbc623ddd4bdcc3fc3d6953badb9
Thorsten Otto <admin@tho-otto.de>
2021-10-15 13:20:18 +0200

    stoslist: add commands from extensions

--- tools/stoslist.c
+++ tools/stoslist.c
@@ -25,6 +25,11 @@ struct token {
 	unsigned char val1;
 	unsigned char val2;
 };
+struct extension {
+	char extension;
+	unsigned char opcode;
+	const char *name;
+};
 
 struct token const tokens[] = {
 	{ "to", T_to, 0 },
@@ -400,6 +405,542 @@ static struct token const extinstructions[] = {
 	{ NULL, 0, 0 }
 };
 
+static struct extension const external_extensions[] = {
+	/*
+    Quartet: 3 commands
+    ' TODO: Add Blitter commands. Difficult due to some 4 word commands and conflicts.
+    ' NOTE: Some extensions have too many conflicting keywords with more popular extensions, e.g. Quick and STE Sound.
+    */
+    /* Compact: 2 commands */
+    { 'A', 0x80, "unpack" },
+    { 'A', 0x81, "pack" },
+
+    /* STOS Compiler: 5 commands */
+    { 'C', 0x80, "compad" },
+    { 'C', 0x82, "comptest off" },
+    { 'C', 0x84, "comptest on" },
+    { 'C', 0x86, "comptest always" },
+    { 'C', 0x88, "comptest" },
+
+    /* STOS Maestro: 20 commands */
+	{ 'D', 0x80, "sound init" },
+	{ 'D', 0x81, "sample" },
+	{ 'D', 0x82, "samplay" },
+	{ 'D', 0x83, "samplace" },
+	{ 'D', 0x84, "samspeed manual" },
+	{ 'D', 0x86, "samspeed auto" },
+	{ 'D', 0x88, "samspeed" },
+	{ 'D', 0x8a, "samstop" },
+	{ 'D', 0x8c, "samloop off" },
+	{ 'D', 0x8e, "samloop on" },
+	{ 'D', 0x90, "samdir forward" },
+	{ 'D', 0x92, "samdir backward" },
+	{ 'D', 0x94, "samsweep on" },
+	{ 'D', 0x96, "samsweep off" },
+	{ 'D', 0x98, "samraw" },
+	{ 'D', 0x9a, "samrecord" },
+	{ 'D', 0x9c, "samcopy" },
+	{ 'D', 0x9e, "sammusic" },
+	{ 'D', 0xa2, "samthru" },
+	{ 'D', 0xa4, "sambank" },
+
+	/* STOS 3D: 59 commands */
+	{ 'S', 0x80, "td priority" },
+	{ 'S', 0x81, "td visible" },
+	{ 'S', 0x82, "td load" },
+	{ 'S', 0x83, "td range" },
+	{ 'S', 0x84, "td clear all" },
+	{ 'S', 0x85, "td position x" },
+	{ 'S', 0x86, "td object" },
+	{ 'S', 0x87, "td position y" },
+	{ 'S', 0x88, "td screen height" },
+	{ 'S', 0x89, "td position z" },
+	{ 'S', 0x8a, "td kill" },
+	{ 'S', 0x8b, "td attitude a" },
+	{ 'S', 0x8c, "td move x" },
+	{ 'S', 0x8d, "td attitude b" },
+	{ 'S', 0x8e, "td move y" },
+	{ 'S', 0x8f, "td attitude b" },
+	{ 'S', 0x90, "td move z" },
+	{ 'S', 0x91, "td collide" },
+	{ 'S', 0x92, "td move rel" },
+	{ 'S', 0x93, "td zone x" },
+	{ 'S', 0x94, "td move" },
+	{ 'S', 0x95, "td zone y" },
+	{ 'S', 0x96, "td angle a" },
+	{ 'S', 0x97, "td zone z" },
+	{ 'S', 0x98, "td angle b" },
+	{ 'S', 0x99, "td zone r" },
+	{ 'S', 0x9a, "td angle c" },
+	{ 'S', 0x9b, "td world x" },
+	{ 'S', 0x9c, "td angle rel" },
+	{ 'S', 0x9d, "td world y" },
+	{ 'S', 0x9e, "td angle" },
+	{ 'S', 0x9f, "td world z" },
+	/* 0xa0 missing */
+	{ 'S', 0xa1, "td view x" },
+	{ 'S', 0xa2, "td delete zone" },
+	{ 'S', 0xa3, "td view y" },
+	{ 'S', 0xa4, "td redraw" },
+	{ 'S', 0xa5, "td view z" },
+	{ 'S', 0xa6, "td set zone" },
+	{ 'S', 0xa7, "td screen x" },
+	{ 'S', 0xa8, "td cls" },
+	{ 'S', 0xa9, "td screen y" },
+	{ 'S', 0xaa, "td background" },
+	{ 'S', 0xab, "td bearing a" },
+	{ 'S', 0xac, "td dir" },
+	{ 'S', 0xad, "td bearing b" },
+	{ 'S', 0xae, "td set colour" },
+	{ 'S', 0xaf, "td bearing r" },
+	{ 'S', 0xb0, "td anim rel" },
+	{ 'S', 0xb1, "td anim point x" },
+	{ 'S', 0xb2, "td init" },
+	{ 'S', 0xb3, "td anim point y" },
+	{ 'S', 0xb4, "td face" },
+	{ 'S', 0xb5, "td anim point z" },
+	{ 'S', 0xb6, "td forward" },
+	{ 'S', 0xb7, "td advanced" },
+	/* 0xb8 missing */
+	{ 'S', 0xb9, "td debug" },
+	{ 'S', 0xba, "td anim" },
+	/* 0xbb missing */
+	{ 'S', 0xbc, "td surface points" },
+	/* 0xbd missing */
+	{ 'S', 0xbe, "td surface" },
+
+    /* Extra: 61 commands */
+	{ 'Z', 0x80, "extra" },
+	{ 'Z', 0x81, "prntr" },
+	{ 'Z', 0x82, "caps lock on" },
+	{ 'Z', 0x83, "ndrv" },
+	{ 'Z', 0x84, "caps lock on" },
+	{ 'Z', 0x85, "write protected" },
+	{ 'Z', 0x86, "swp col" },
+	{ 'Z', 0x87, "gemdos version$" },
+	{ 'Z', 0x88, "screen dump" },
+	{ 'Z', 0x89, "os version$" },
+	{ 'Z', 0x8a, "disk verify on" },
+	{ 'Z', 0x8b, "left shift key" },
+	{ 'Z', 0x8c, "disk verify off" },
+	{ 'Z', 0x8d, "right shift key" },
+	{ 'Z', 0x8e, "fmt disc" },
+	{ 'Z', 0x8f, "cntrl key" },
+	{ 'Z', 0x90, "cpy disc" },
+	{ 'Z', 0x91, "alt key" },
+	{ 'Z', 0x92, "visible input" },
+	{ 'Z', 0x93, "caps lock" },
+	{ 'Z', 0x94, "blur" },
+	{ 'Z', 0x95, "screen hz" },
+	{ 'Z', 0x96, "set rs232" },
+	{ 'Z', 0x97, "media status" },
+	{ 'Z', 0x98, "deshade" },
+	{ 'Z', 0x99, "power" },
+	{ 'Z', 0x9a, "set printer data" },
+	{ 'Z', 0x9b, "cartridge input" },
+	{ 'Z', 0x9c, "hrev" },
+	{ 'Z', 0x9d, "disc sides" },
+	{ 'Z', 0x9e, "set fattrib" },
+	{ 'Z', 0x9f, "disc tracks" },
+	/* 0xa0 missing */
+	{ 'Z', 0xa1, "cookie _cpu" },
+	{ 'Z', 0xa2, "pal inverse" },
+	{ 'Z', 0xa3, "disc spt" },
+	{ 'Z', 0xa4, "set screen hz" },
+	/* 0xa5 missing */
+	{ 'Z', 0xa6, "pal change" },
+	{ 'Z', 0xa7, "fattrib" },
+	{ 'Z', 0xa8, "ppsc" },
+	{ 'Z', 0xa9, "cookie _mch" },
+	{ 'Z', 0xaa, "opaque screen" },
+	{ 'Z', 0xab, "compact" },
+	{ 'Z', 0xac, "vrev" },
+	{ 'Z', 0xad, "mem config" },
+	{ 'Z', 0xae, "desquash" },
+	{ 'Z', 0xaf, "disc verify" },
+	{ 'Z', 0xb0, "del" },
+	{ 'Z', 0xb1, "disc size" },
+	{ 'Z', 0xb2, "pload" },
+	{ 'Z', 0xb3, "disc free" },
+	{ 'Z', 0xb4, "disable mouse" },
+	{ 'Z', 0xb5, "disc used" },
+	{ 'Z', 0xb6, "enable mouse" },
+	{ 'Z', 0xb7, "vtab" },
+	{ 'Z', 0xb8, "screen squash" },
+	{ 'Z', 0xb9, "ltab" },
+	{ 'Z', 0xba, "label disc" },
+	{ 'Z', 0xbb, "setup of printer" },
+	{ 'Z', 0xbc, "rmv dup col" },
+	/* 0xbd missing */
+	{ 'Z', 0xbe, "ren" },
+	{ 'Z', 0xbf, "fmt text" },
+
+    /* Control: 61 commands */
+	{ 'W', 0x80, "switch on" },
+	{ 'W', 0x81, "case" },
+	{ 'W', 0x82, "switch off" },
+	{ 'W', 0x83, "otherwise" },
+	{ 'W', 0x84, "cmove" },
+	/* 0x85 missing */
+	{ 'W', 0x86, "write" },
+	{ 'W', 0x87, "parallel" },
+	{ 'W', 0x88, "cremember" },
+	{ 'W', 0x89, "para fire" },
+	{ 'W', 0x8a, "crecall" },
+	{ 'W', 0x8b, "add" },
+	{ 'W', 0x8c, "ctrl" },
+	{ 'W', 0x8d, "test megazone" },
+	{ 'W', 0x8e, "para on" },
+	{ 'W', 0x8f, "para up" },
+	{ 'W', 0x90, "para off" },
+	{ 'W', 0x91, "para down" },
+	{ 'W', 0x92, "init megazone" },
+	{ 'W', 0x93, "para left" },
+	{ 'W', 0x94, "set megazone" },
+	{ 'W', 0x95, "klatu" },
+	{ 'W', 0x96, "range megazone" },
+	{ 'W', 0x97, "exist$" },
+	{ 'W', 0x98, "map write" },
+	/* 0x99 missing */
+	{ 'W', 0x9a, "spread" },
+	{ 'W', 0x9b, "para right" },
+	{ 'W', 0x9c, "brdr remove" },
+	{ 'W', 0x9d, "crack pac" },
+	{ 'W', 0x9e, "crack unpac" },
+	{ 'W', 0x9f, "map address" },
+	{ 'W', 0xa0, "quick screen" },
+	{ 'W', 0xa1, "klatu" },
+	{ 'W', 0xa2, "font" },
+	{ 'W', 0xa3, "map h" },
+	{ 'W', 0xa4, "image put" },
+	{ 'W', 0xa5, "map w" },
+	{ 'W', 0xa6, "screen size" },
+	{ 'W', 0xa7, "klatu" },
+	{ 'W', 0xa8, "hscroll" },
+	{ 'W', 0xa9, "image width" },
+	{ 'W', 0xaa, "image palette" },
+	{ 'W', 0xab, "image height" },
+	{ 'W', 0xac, "many image" },
+	{ 'W', 0xad, "map read" },
+	{ 'W', 0xae, "set clip" },
+	{ 'W', 0xaf, "klatu" },
+	{ 'W', 0xb0, "turbocopy" },
+	{ 'W', 0xb1, "klatu" },
+	{ 'W', 0xb2, "bigcls" },
+	{ 'W', 0xb3, "inside" },
+	{ 'W', 0xb4, "bigcopy" },
+	{ 'W', 0xb5, "image collide" },
+	{ 'W', 0xb6, "screen offset" },
+	{ 'W', 0xb7, "image mcollide" },
+	{ 'W', 0xb8, "cylinder" },
+	{ 'W', 0xb9, "klatu" },
+	{ 'W', 0xba, "image map" },
+	{ 'W', 0xbb, "jagjoy" },
+	{ 'W', 0xbc, "set map" },
+	{ 'W', 0xbd, "klatu" },
+	{ 'W', 0xbe, "image offset" },
+
+    /* Misty: 21 commands */
+	{ 'M', 0x80, "fastcopy" },
+	{ 'M', 0x81, "col" },
+	{ 'M', 0x82, "floprd" },
+	{ 'M', 0x83, "mediach" },
+	{ 'M', 0x84, "flopwrt" },
+	{ 'M', 0x85, "hardkey" },
+	{ 'M', 0x86, "dot" },
+	{ 'M', 0x87, "ndrv" },
+	{ 'M', 0x88, "mouseoff" },
+	{ 'M', 0x89, "freq" },
+	{ 'M', 0x8a, "mouseon" },
+	{ 'M', 0x8b, "resvalid" },
+	{ 'M', 0x8c, "skopy" },
+	{ 'M', 0x8d, "aesin" },
+	{ 'M', 0x8e, "setrtim" },
+	{ 'M', 0x8f, "rtim" },
+	{ 'M', 0x90, "warmboot" },
+	{ 'M', 0x91, "blitter" },
+	{ 'M', 0x92, "silence" },
+	{ 'M', 0x93, "kbshift" },
+	{ 'M', 0x94, "kopy" },
+
+    /* Missing Link: 33 commands (link1) */
+	{ 'Q', 0x80, "landscape" },
+	{ 'Q', 0x81, "overlap" },
+	{ 'Q', 0x82, "bob" },
+	{ 'Q', 0x83, "map toggle" },
+	{ 'Q', 0x84, "wipe" },
+	{ 'Q', 0x85, "boundary" },
+	{ 'Q', 0x86, "tile" },
+	{ 'Q', 0x87, "palt" },
+	{ 'Q', 0x88, "world" },
+	{ 'Q', 0x89, "musauto" },
+	{ 'Q', 0x8a, "musplay" },
+	{ 'Q', 0x8b, "which block" },
+	{ 'Q', 0x8c, "relocate" },
+	{ 'Q', 0x8d, "p left" },
+	{ 'Q', 0x8e, "p on" },
+	{ 'Q', 0x8f, "p joy" },
+	{ 'Q', 0x90, "p stop" },
+	{ 'Q', 0x91, "p up" },
+	{ 'Q', 0x92, "set block" },
+	{ 'Q', 0x93, "p right" },
+	{ 'Q', 0x94, "palsplit" },
+	{ 'Q', 0x95, "p down" },
+	{ 'Q', 0x96, "floodpal" },
+	{ 'Q', 0x97, "p fire" },
+	{ 'Q', 0x98, "digi play" },
+	{ 'Q', 0x99, "string" },
+	{ 'Q', 0x9a, "samsign" },
+	{ 'Q', 0x9b, "depack" },
+	{ 'Q', 0x9c, "replace blocks" },
+	{ 'Q', 0x9d, "dload" },
+	{ 'Q', 0x9e, "display pc1" },
+	{ 'Q', 0x9f, "dsave" },
+	{ 'Q', 0xa0, "honesty" },
+
+    /* Missing Link: 28 commands (link2) */
+	{ 'R', 0x80, "joey" },
+	{ 'R', 0x81, "b height" },
+	{ 'R', 0x82, "blit" },
+	{ 'R', 0x83, "b width" },
+	{ 'R', 0x84, "spot" },
+	{ 'R', 0x85, "block amount" },
+	{ 'R', 0x86, "reflect" },
+	{ 'R', 0x87, "compstate" },
+	{ 'R', 0x88, "mozaic" },
+	{ 'R', 0x89, "x limit" },
+	{ 'R', 0x8a, "xy block" },
+	{ 'R', 0x8b, "y limit" },
+	{ 'R', 0x8c, "text" },
+	{ 'R', 0x8d, "mostly harmless" },
+	{ 'R', 0x8e, "wash" },
+	{ 'R', 0x8f, "real length" },
+	{ 'R', 0x90, "reboot" },
+	{ 'R', 0x91, "brightest" },
+	{ 'R', 0x92, "bank load" },
+	{ 'R', 0x93, "bank length" },
+	{ 'R', 0x94, "bank copy" },
+	{ 'R', 0x95, "bank size" },
+	{ 'R', 0x96, "m blit" },
+	{ 'R', 0x97, "win block amount" },
+	{ 'R', 0x98, "replace range" },
+	{ 'R', 0x99, "empty" },
+	{ 'R', 0x9a, "win replace blocks" },
+	{ 'R', 0x9b, "empty2" },
+	{ 'R', 0x9c, "win replace range" },
+	{ 'R', 0x9d, "empty3" },
+	{ 'R', 0x9e, "win xy block" },
+
+    /* Missing Link: 13 commands (link3) */
+	{ 'S', 0x80, "many add" },
+	{ 'S', 0x81, "many overlap" },
+	{ 'S', 0x82, "many sub" },
+	{ 'S', 0x83, "function2" },
+	{ 'S', 0x84, "many bob" },
+	{ 'S', 0x85, "function3" },
+	{ 'S', 0x86, "many joey" },
+	{ 'S', 0x87, "hertz" },
+	{ 'S', 0x88, "set hertz" },
+	{ 'S', 0x89, "function4" },
+	{ 'S', 0x8a, "many inc" },
+	{ 'S', 0x8b, "function5" },
+	{ 'S', 0x8c, "many dec" },
+	{ 'S', 0x8d, "function6" },
+	{ 'S', 0x8e, "raster" },
+	{ 'S', 0x8f, "function7" },
+	{ 'S', 0x90, "bullet" },
+	{ 'S', 0x91, "function8" },
+	{ 'S', 0x92, "many bullet" },
+	{ 'S', 0x93, "function9" },
+	{ 'S', 0x94, "many spot" },
+
+    /* Ninja Tracker: 9 commands */
+	{ 'Q', 0x80, "track play" },
+	{ 'Q', 0x81, "vu meter" },
+	{ 'Q', 0x82, "track frequency" },
+	{ 'Q', 0x83, "track pos" },
+	{ 'Q', 0x84, "track info" },
+	{ 'Q', 0x85, "track pattern" },
+	/* 0x86 missing */
+	{ 'Q', 0x87, "track key" },
+	{ 'Q', 0x88, "track unpack" },
+	{ 'Q', 0x89, "track name" },
+
+    /* GPB: 32 commands */
+	{ 'P', 128, "lights on" },
+	{ 'P', 129, "pready" },
+	{ 'P', 130, "lights off" },
+	{ 'P', 131, "xpen" },
+	{ 'P', 132, "fastwipe" },
+	{ 'P', 133, "paktype" },
+	{ 'P', 134, "dac volume" },
+	{ 'P', 135, "even" },
+	{ 'P', 136, "setpal" },
+	{ 'P', 137, "setprt" },
+	{ 'P', 138, "d crunch" },
+	{ 'P', 139, "eplace" },
+	{ 'P', 140, "elite unpack" },
+	{ 'P', 141, "foffset" },
+	{ 'P', 142, "estop" },
+	{ 'P', 143, "jar" },
+	{ 'P', 144, "mirror" },
+	{ 'P', 145, "percent" },
+	{ 'P', 146, "tiny unpack" },
+	{ 'P', 147, "paksize" },
+	{ 'P', 148, "treble" },
+	{ 'P', 149, "special key" },
+	{ 'P', 150, "bass" },
+	{ 'P', 151, "fstart" },
+	{ 'P', 152, "hcopy" },
+	{ 'P', 153, "flength" },
+	{ 'P', 154, "ca unpack" },
+	{ 'P', 155, "ca pack" },
+	{ 'P', 156, "bcls" },
+	{ 'P', 157, "cookie" },
+	{ 'P', 158, "eplay" },
+	{ 'P', 159, "ypen" },
+
+    /* STOS Tracker: 9 commands (track_10) */
+	{ 'T', 0x80, "track load" },
+	{ 'T', 0x81, "track scan" },
+	{ 'T', 0x82, "track bank" },
+	{ 'T', 0x83, "track vu" },
+	{ 'T', 0x84, "track play" },
+	/* 0x85 missing */
+	{ 'T', 0x86, "track key" },
+	/* 0x87 missing */
+	{ 'T', 0x88, "track volume" },
+	/* 0x89 missing */
+	{ 'T', 0x8a, "track tempo" },
+	/* 0x8b missing */
+	{ 'T', 0x8c, "track stop" },
+
+    /* STOS Tracker: 9 commands (track_7/track_14) */
+	{ 'S', 0x80, "track load" },
+	{ 'S', 0x81, "track scan" },
+	{ 'S', 0x82, "track bank" },
+	{ 'S', 0x83, "track vu" },
+	{ 'S', 0x84, "track play" },
+	/* 0x85 missing */
+	{ 'S', 0x86, "track key" },
+	/* 0x87 missing */
+	{ 'S', 0x88, "track volume" },
+	/* 0x89 missing */
+	{ 'S', 0x8a, "track tempo" },
+	/* 0x8b missing */
+	{ 'S', 0x8c, "track stop" },
+
+    /* MIDI: 5 commands */
+	{ 'M', 0x80, "midi on" },
+	{ 'M', 0x81, "midi in" },
+	{ 'M', 0x82, "midi off" },
+	/* 0x83 missing */
+	{ 'M', 0x84, "midi out" },
+	/* 0x85 missing */
+	{ 'M', 0x86, "about musician" },
+
+    /* Stars: 4 commands */
+	{ 'H', 0x80, "set stars" },
+	/* $81 missing */
+	{ 'H', 0x82, "go stars" },
+	/* $83 missing */
+	{ 'H', 0x84, "wipe stars on" },
+	/* $85 missing */
+	{ 'H', 0x86, "wipe stars off" },
+
+	/* STE extension: 35 commands */
+	{ 'F', 0x80, "sticks on" },
+	{ 'F', 0x81, "stick1" },
+	{ 'F', 0x82, "sticks off" },
+	{ 'F', 0x83, "stick2" },
+	{ 'F', 0x84, "dac convert" },
+	{ 'F', 0x85, "l stick" },
+	{ 'F', 0x86, "dac raw" },
+	{ 'F', 0x87, "r stick" },
+	{ 'F', 0x88, "dac speed" },
+	{ 'F', 0x89, "u stick" },
+	{ 'F', 0x8a, "dac stop" },
+	{ 'F', 0x8b, "d stick" },
+	{ 'F', 0x8c, "dac m volume" },
+	{ 'F', 0x8d, "f stick" },
+	{ 'F', 0x8e, "dac l volume" },
+	{ 'F', 0x8f, "light x" },
+	{ 'F', 0x90, "dac r volume" },
+	{ 'F', 0x91, "light y" },
+	{ 'F', 0x92, "dac treble" },
+	{ 'F', 0x93, "ste" },
+	{ 'F', 0x94, "dac bass" },
+	{ 'F', 0x95, "e color" },
+	{ 'F', 0x96, "dac mix on" },
+	{ 'F', 0x97, "hard physic" },
+	{ 'F', 0x98, "dac mix off" },
+	/* 0x99 missing */
+	{ 'F', 0x9a, "dac mono" },
+	/* 0x9b missing */
+	{ 'F', 0x9c, "dac stereo" },
+	/* 0x9d missing */
+	{ 'F', 0x9e, "dac loop on" },
+	/* 0x9f missing */
+	{ 'F', 0xa0, "dac loop off" },
+	/* 0xa1 missing */
+	{ 'F', 0xa2, "e palette" },
+	/* 0xa3 missing */
+	{ 'F', 0xa4, "e colour" },
+	/* 0xa5 missing */
+	{ 'F', 0xa6, "hard screen size" },
+	/* 0xa7 missing */
+	{ 'F', 0xa8, "hard screen offset" },
+	/* 0xa9 missing */
+	{ 'F', 0xaa, "hard screen offset" },
+	/* 0xab missing */
+	{ 'F', 0xac, "hard screen offset" },
+
+	/* STORM/GBP blitter extension: 14 commands */
+	{ 'G', 0x80, "blit s inc" },
+	/* 0x81 missing */
+	{ 'G', 0x82, "blit d inc" },
+	/* 0x83 missing */
+	{ 'G', 0x84, "blit address" },
+	/* 0x85 missing */
+	{ 'G', 0x86, "blit mask" },
+	/* 0x87 missing */
+	{ 'G', 0x88, "blit count" },
+	/* 0x89 missing */
+	{ 'G', 0x8a, "blit hop" },
+	/* 0x8b missing */
+	{ 'G', 0x8c, "blit op" },
+	/* 0x8d missing */
+	{ 'G', 0x8e, "blit skew" },
+	/* 0x8f missing */
+	{ 'G', 0x90, "blit nfsr" },
+	/* 0x91 missing */
+	{ 'G', 0x92, "blit fxsr" },
+	/* 0x93 missing */
+	{ 'G', 0x94, "blit line" },
+	/* 0x95 missing */
+	{ 'G', 0x96, "blit smudge" },
+	/* 0x97 missing */
+	{ 'G', 0x98, "blit hog" },
+	/* 0x99 missing */
+	{ 'G', 0x9a, "blit it" },
+
+    /* Cyber: 2 commands */
+	{ 'Y', 0x80, "cyber" },
+	/* 0x81 missing */
+	{ 'Y', 0x82, "view cyber" },
+    
+    /* STOS Squasher: 2 commands */
+	{ 'E', 0x80, "unsquash" },
+	{ 'E', 0x81, "squash" },
+
+    /* Protracker: 3 commands */
+	{ 'P', 0x80, "propack" },
+	{ 'P', 0x81, "mpack" },
+	{ 'P', 0x83, "munpack" },
+
+	{ 0, 0, NULL }
+};
+
 static int upperflag = FALSE;
 
 #define TOUPPER(c) (upperflag ? ((c) >= 'a' && (c) <= 'z' ? (c) - ('a' - 'A') : (c)) : (c))
@@ -446,20 +987,23 @@ static int32_t readbe32(FILE *fp)
 }
 
 
+static void print_upper(const char *str, FILE *out)
+{
+	while (*str)
+	{
+		putc(TOUPPER(*str), out);
+		str++;
+	}
+}
+
+
 static void print_extfunc(unsigned char opcode, unsigned char opcode2, const struct token *tokens, FILE *out)
 {
-	const char *p;
-	
 	while (tokens->name != NULL)
 	{
 		if (tokens->val1 == opcode && tokens->val2 == opcode2)
 		{
-			p = tokens->name;
-			while (*p)
-			{
-				putc(TOUPPER(*p), out);
-				p++;
-			}
+			print_upper(tokens->name, out);
 			return;
 		}
 		tokens++;
@@ -470,23 +1014,33 @@ static void print_extfunc(unsigned char opcode, unsigned char opcode2, const str
 
 static void print_token(unsigned char opcode, const struct token *tokens, FILE *out)
 {
-	const char *p;
-	
 	while (tokens->name != NULL)
 	{
 		if (tokens->val1 == opcode)
 		{
-			p = tokens->name;
-			while (*p)
-			{
-				putc(TOUPPER(*p), out);
-				p++;
-			}
+			print_upper(tokens->name, out);
 			return;
 		}
 		tokens++;
 	}
 	fputs("ILLEGAL", out);
+}
+
+
+static void print_external_extension(char extension, unsigned char opcode, FILE *out)
+{
+	const struct extension *ext = external_extensions;
+	
+	while (ext->name != NULL)
+	{
+		if (ext->extension == extension && ext->opcode == opcode)
+		{
+			print_upper(ext->name, out);
+			return;
+		}
+		ext++;
+	}
+	fprintf(out, "extension #%c(0x%02x)", extension, opcode);
 }
 
 
@@ -681,16 +1235,15 @@ static int listfile(FILE *fp)
 			} else if (opcode == T_extensioninst || opcode == T_extensionfunc)
 			{
 				/* dt11y */
-				int extensiono;
-				int funcno;
+				int extension;
 				
-				extensiono = fgetc(fp);
-				funcno = fgetc(fp);
+				extension = fgetc(fp);
+				opcode = fgetc(fp);
 				linelength -= 2;
 				/*
 				 * TODO: load extensions
 				 */
-				fprintf(out, "extension #%c(0x%02x)", 'A' + extensiono, funcno);
+				print_external_extension('A' + extension, opcode, out);
 			} else
 			{
 				/* NORMAL token */
